2dc06bc8a8e7609d099e848c9f7d5b166ce33b99
Christina Mills <chrimills@gmail.com>
2017-10-31 13:57:00 -0700

    Nooo, nooo, its working

--- test/e2e/experiences.test.js
+++ test/e2e/experiences.test.js
@@ -1,31 +1,41 @@
 const { assert } = require('chai');
 const request = require('./request');
-const Experience = require('../../lib/models/Experience');
+// const Experience = require('../../lib/models/Experience');
 // const ensureAuth = require('../../lib/utils/ensure-auth');
+const tokenService = require('../../lib/utils/token-service');
+// const { Schema } = require('mongoose');
 
 
 describe('experience API', () => {
     
-    beforeEach(() => Experience.collection.drop());
+    // beforeEach(() => Experience.collection.drop());
 
     let token = '';
+    let testUser = {
+        name: 'Persona Peters',
+        email: 'person@email.com', 
+        password: 'personpass'
+    };
 
     beforeEach(() => {
         return request
-            .post('/signup')
-            .send({
-                name: 'Persona Peters',
-                email: 'person@email.com'
+            .post('/api/auth/signup')
+            .send(testUser)
+            .then(({ body }) => {
+                token = body.token;
+                return tokenService.verify(token);
             })
-            .then(({ body }) => token = body.token);
+            .then((res) => {
+                testUser._id = res.id;
+            });
     });
 
-    it('posts an experience', () => {
-        return request.post('/experiences')
+    it.only('posts an experience', () => {
+        return request.post('/api/experiences')
             .set({Authorization: token })
             .send({
-                country: 'Spain',
-                user: 'Gina',
+                country: '59f8a92964922f0d70faf32d',
+                user: testUser._id,
                 comment: 'OMFG tapas!',
                 rating: '5'
             })
